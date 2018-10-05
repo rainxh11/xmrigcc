764de83274bd489139c8627b79fda00895982f13
Ben Gräf <ben@gra3f.de>
2018-10-05 15:10:14 +0200

    Fixed device by 0 issue when using safe mode #173

--- src/Cpu.cpp
+++ src/Cpu.cpp
@@ -86,9 +86,9 @@ void CpuImpl::optimizeParameters(size_t& threadsCount, size_t& hashFactor,
         if (threadsCount > maximumReasonableThreadCount) {
             threadsCount = maximumReasonableThreadCount;
         }
-        if (hashFactor > maximumReasonableFactor / threadsCount) {
+        if (threadsCount > 0 && hashFactor > maximumReasonableFactor / threadsCount) {
             hashFactor = std::min(maximumReasonableFactor / threadsCount, maximumReasonableHashFactor);
-            hashFactor   = std::max(hashFactor, static_cast<size_t>(1));
+            hashFactor  = std::max(hashFactor, static_cast<size_t>(1));
         }
     }
 
@@ -106,9 +106,10 @@ void CpuImpl::optimizeParameters(size_t& threadsCount, size_t& hashFactor,
         }
         threadsCount = std::max(threadsCount, static_cast<size_t>(1));
     }
+
     if (hashFactor == 0) {
         hashFactor = std::min(maximumReasonableHashFactor, maximumReasonableFactor / threadsCount);
-        hashFactor   = std::max(hashFactor, static_cast<size_t>(1));
+        hashFactor = std::max(hashFactor, static_cast<size_t>(1));
     }
 }
 
